1a7e6d0a9ced80e56765c336f25c83944e3cac4a
“[negar <miralaienegar@gmail.com>
2022-01-03 12:35:59 +0330

    created ProductsPage component and its router

--- shopping-cart-1/src/router/index.js
+++ shopping-cart-1/src/router/index.js
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Home from '../views/Home.vue'
+import ProductsPage from '../views/ProductsPage.vue'
 
 const routes = [
   {
@@ -9,11 +10,12 @@ const routes = [
   },
   {
     path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    name: 'About'
+  },
+  {
+    path: '/products',
+    name: 'ProductsPage',
+    component: ProductsPage
   }
 ]
 
--- shopping-cart-1/src/views/Home.vue
+++ shopping-cart-1/src/views/Home.vue
@@ -1,13 +1,14 @@
 <template>
   <div class="home">
-    
+    <ProductsPage/>
   </div>
 </template>
 
 <script>
+import ProductsPage from '../views/ProductsPage.vue'
 
 export default {
   name: 'Home',
-  components: {}
+  components: {ProductsPage}
 }
 </script>
