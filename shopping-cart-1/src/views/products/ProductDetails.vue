0216b69aa9b16a73cb3cb42f170c6ff82ad4ac64
“[negar <miralaienegar@gmail.com>
2022-01-03 17:00:53 +0330

    made product detail appear when ever a product card is clicked

---
+++ shopping-cart-1/src/views/products/ProductDetails.vue
@@ -0,0 +1,40 @@
+<template>
+  <section v-if="isShowProductDetails">
+      <p>{{product.name}}</p>
+    </section>
+</template>
+
+<script>
+import ProductCard from './productCard.vue'
+
+export default {
+  name: "ProductDetails",
+  components: {ProductCard},
+  props: ["product", "isShowProductDetails"],
+  data() {
+    return {};
+  },
+  mounted() {
+      console.log("mounted")
+    console.log(this.product);
+  },
+};
+</script>
+
+<style scoped>
+section {
+  background-color: red;
+  /* position:fixed;
+    top: 0;
+    right: 0;
+    bottom: 0;
+    left: 0; */
+  width: 100%;
+  height: 100%;
+  z-index: 5;
+}
+
+p {
+  background-color: rgb(18, 126, 8);
+}
+</style>
--- shopping-cart-1/src/views/products/ProductsPage.vue
+++ shopping-cart-1/src/views/products/ProductsPage.vue
@@ -1,9 +1,15 @@
 <template>
-<!-- <h2>Products:</h2> -->
+  <!-- <h2>Products:</h2> -->
   <section>
-    <section v-for="product in productsList" :key="product.id" class="products">
-      <productCard :product="product"/>
+    <section
+      v-for="product in productsList"
+      :key="product.id"
+      @click="showProductDetails(product)"
+      class="products"
+    >
+      <productCard :product="product" />
     </section>
+    <ProductDetails :isShowProductDetails="isShowProductDetails" :product="clickedProduct" />
   </section>
 </template>
 
@@ -11,24 +17,36 @@
 import ProductCard from "./productCard.vue";
 import { mapState } from "vuex";
 import productsList from "../../data/productsList";
+import ProductDetails from "./ProductDetails.vue";
 
 export default {
   name: "ProductsPage",
-  components: { ProductCard },
-    computed:{
-      ...mapState([
-          "productsList"
-      ])},
+  components: { ProductCard, ProductDetails },
+  data() {
+    return {
+      isShowProductDetails: false,
+      clickedProduct: null
+    };
+  },
+  computed: {
+    ...mapState(["productsList"]),
+  },
+  methods: {
+    showProductDetails(product) {
+      this.isShowProductDetails = true;
+      this.clickedProduct = product;
+    },
+  },
 };
 </script>
 
 <style>
-    .products{
-        display: flex;
-        flex-direction: column;
-        justify-content: center;
-        align-items: center;
-        background-color:rgb(223, 201, 243);
-        color: white;
-    }
+.products {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  background-color: rgb(223, 201, 243);
+  color: white;
+}
 </style>
