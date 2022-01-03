8a16db2d32ac1f5c90f69fbb11f743ad902cc2f1
“[negar <miralaienegar@gmail.com>
2022-01-03 15:29:27 +0330

    created shortDescription using getters in vuex

--- shopping-cart-1/src/store/index.js
+++ shopping-cart-1/src/store/index.js
@@ -1,13 +1,22 @@
 import { createStore } from 'vuex'
 import productsList from '../data/productsList'
 
+// findProduct = () =>{
+
+// }
+
 export default createStore({
   state: {
     productsList: productsList
   },
-  // getters:{
-  //   return 
-  // },
+  getters:{
+    shortDescription: state => product =>{
+      return state.productsList
+        .find((productListItem) => productListItem.id === product.id)
+        .description.substring(0, 50);
+      // return product.description.substring(0, 50);
+    } 
+  },
   mutations: {},
   actions: {},
   modules: {},
--- shopping-cart-1/src/views/products/productCard.vue
+++ shopping-cart-1/src/views/products/productCard.vue
@@ -3,13 +3,25 @@
     <p>{{product.price}}</p>
     <p>{{product.category}}</p>
     <p>{{product.quantity}}</p>
-    <small>{{product.description}}</small>
+    <!-- <small>{{product.description}}</small> -->
+    <small>{{shortDescription(product)}}</small>
 </template>
 
+
 <script>
+import {mapGetters} from 'vuex'
+
 export default {
     name: "productCard",
-    props:['product']
+    props:['product'],
+    computed:{
+        // shortDescription(){
+        //     return this.$store.getters.shortDescription
+        // }
+        ...mapGetters([
+            'shortDescription'
+        ]),
+    }
     
 }
 </script>
