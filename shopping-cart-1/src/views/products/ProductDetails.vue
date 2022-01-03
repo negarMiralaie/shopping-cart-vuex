63d65ea908a71cf3a4e3dabab0c293d5a15e1a5e
“[negar <miralaienegar@gmail.com>
2022-01-03 18:51:58 +0330

    styled ProductDetails

--- shopping-cart-1/src/views/products/ProductDetails.vue
+++ shopping-cart-1/src/views/products/ProductDetails.vue
@@ -1,40 +1,50 @@
 <template>
-  <section v-if="isShowProductDetails">
-      <p>{{product.name}}</p>
+  <section v-if="isShowProductDetails" class="productDetailsBg">
+    <section class="productDetails">
+      <p>{{ product.name }}</p>
+      <p>{{product.price}}</p>
+      <p>{{product.quantity}}</p>
+      <p>{{product.category}}</p>
+      <p>{{product.description}}</p>
     </section>
+  </section>
 </template>
 
 <script>
-import ProductCard from './productCard.vue'
+import ProductCard from "./productCard.vue";
 
 export default {
   name: "ProductDetails",
-  components: {ProductCard},
+  components: { ProductCard },
   props: ["product", "isShowProductDetails"],
   data() {
     return {};
   },
   mounted() {
-      console.log("mounted")
+    console.log("mounted");
     console.log(this.product);
   },
 };
 </script>
 
 <style scoped>
-section {
-  background-color: red;
-  /* position:fixed;
-    top: 0;
-    right: 0;
-    bottom: 0;
-    left: 0; */
+.productDetailsBg{
+  background-color: rgb(126, 119, 119);
+  position: fixed;
+  top: 0;
+  right: 0;
+  bottom: 0;
+  left: 0;
   width: 100%;
   height: 100%;
   z-index: 5;
+  display: grid;
+  place-items: center;
 }
 
-p {
-  background-color: rgb(18, 126, 8);
+.productDetails{
+  background-color:rgb(250, 99, 192);
+  position: fixed;
 }
+
 </style>
