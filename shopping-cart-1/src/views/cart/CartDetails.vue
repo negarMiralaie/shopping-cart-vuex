48a2878377ace13e370dd7d7b107c03c4f7e2862
“[negar <miralaienegar@gmail.com>
2022-01-04 18:27:30 +0330

    styled cart a bit

--- shopping-cart-1/src/views/cart/CartDetails.vue
+++ shopping-cart-1/src/views/cart/CartDetails.vue
@@ -1,19 +1,24 @@
 <template>
-    <div>
-        <h1>This is cart</h1>
-        <DisplayCart/>
-    </div>
+  <div>
+    <h1>This is cart</h1>
+    <DisplayCart class="cart" />
+  </div>
 </template>
 
 <script>
-import DisplayCart from './DisplayCart.vue'
+import DisplayCart from "./DisplayCart.vue";
 
-    export default {
-        name: 'CartDetails',
-        components: {DisplayCart}
-    }
+export default {
+  name: "CartDetails",
+  components: { DisplayCart },
+};
 </script>
 
 <style scoped>
-
+.cart {
+  background-color: rgb(236, 86, 86);
+  display: flex;
+  flex-direction: column;
+  gap: 0.75rem;
+}
 </style>
