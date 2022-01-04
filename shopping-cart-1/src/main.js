6216271962c90b00bbfa41330a099b8331992a67
“[negar <miralaienegar@gmail.com>
2022-01-04 19:24:34 +0330

    added cart icon

--- shopping-cart-1/src/main.js
+++ shopping-cart-1/src/main.js
@@ -2,6 +2,15 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { fas } from "@fortawesome/free-solid-svg-icons";
 
+library.add(fas);
 
-createApp(App).use(store).use(router).mount('#app')
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+
+createApp(App)
+  .use(store)
+  .use(router)
+  .component("font-awesome-icon", FontAwesomeIcon)
+  .mount("#app");
--- shopping-cart-1/src/views/cart/NavCart.vue
+++ shopping-cart-1/src/views/cart/NavCart.vue
@@ -1,30 +1,41 @@
 <template>
-  <section>
+  <section class="navCartContainer">
     <p>Cart</p>
-    <p class="navCartBadge">0</p>
+    <section class="cartIconAndBadgeSec">
+      <font-awesome-icon icon="shopping-cart" class="cartIcon" />
+      <!-- <p class="navCartBadge">0</p> -->
+    </section>
   </section>
 </template>
 
 <script>
 export default {
-    name:'NavCart'
+  name: "NavCart",
 };
 </script>
 
 <style scoped>
-  section{
-    /* background-color:yellowgreen; */
-    display: flex;
-    align-items:start;
-    justify-content:flex-start;
-    height: 100%;
-    position: relative;
-  }
+.navCartContainer {
+  /* background-color:rgb(50, 117, 205); */
+  display: flex;
+  height: 100%;
+}
 
-  .navCartBadge{
-    /* background-color:rgb(248, 40, 25); */
-    position:absolute;
-    /* bottom:15%; */
-    right:0;
-  }
+.cartIconAndBadgeSec {
+  position: relative;
+  /* background-color:yellowgreen; */
+  display: flex;
+  align-items: center;
+}
+
+.cartIcon{
+  /* background:red; */
+}
+
+.navCartBadge {
+  background-color:rgb(248, 40, 25);
+  position: absolute;
+  bottom:15%;
+  right: 0;
+}
 </style>
